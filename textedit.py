2a72d2e2e67d9f1a7f9fa7ac451f336dc289d28a
vidhita <122602734+vidhitamittal@users.noreply.github.com>
2025-10-28 17:14:04 -0400

    added a save button to the textedit

--- textedit.py
+++ textedit.py
@@ -1,7 +1,15 @@
 from tkinter import *
-
+import tkinter.filedialog
 
 root=Tk("Text Editor")
 text=Text(root)
 text.grid()
-root.mainloop()
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation=tkinter.filedialog.asksaveasfilename()
+    file1=open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+button=Button(root, text="Save", command=saveas)
+button.grid() 
